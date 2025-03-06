ffe4ff45bb8c6b26699d3975915904c3c9ed8a77
ElDiegu <eldiegugg@gmail.com>
2025-03-06 20:00:18 +0100

    Implemented Event System from git-ammend tutorial

---
+++ MushyAndCoffe/Assets/_Project/Systems/EventSystem/IEvent.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+namespace EventSystem
+{
+    public interface IEvent { }
+}
---
+++ MushyAndCoffe/Assets/_Project/Systems/EventSystem/PredefinedAssemblyUtil.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace EventSystem
+{
+	public class PredefinedAssemblyUtil
+	{
+		enum AssemblyType 
+		{
+			AssemblyCSharp,
+			AssemblyCSharpFirstPass,
+			AssemblyCSharpEditor,
+			AssemblyCSharpEditorFirstPass
+		}
+		
+		static AssemblyType? GetAssemblyType(string assemblyName) 
+		{
+			return assemblyName switch 
+			{
+				"Assembly-CSharp" => AssemblyType.AssemblyCSharp,
+				"Assembly-CSharp-firstpass" => AssemblyType.AssemblyCSharpFirstPass,
+				"Assembly-CSharp-Editor" => AssemblyType.AssemblyCSharpEditor,
+				"Assembly-CSharp-Editor-firstpass" => AssemblyType.AssemblyCSharpEditorFirstPass,
+				_ => null
+			};
+		}
+		
+		static void AddTypesFromAssembly(Type[] assembly, ICollection<Type> types, Type interfaceType) 
+		{
+			if (assembly == null) return;
+			
+			for (int i = 0; i < assembly.Length; i++) 
+			{
+				Type type = assembly[i];
+				if (type != interfaceType && interfaceType.IsAssignableFrom(type)) 
+				{
+					types.Add(type);
+				}
+			}
+		}
+		
+		public static List<Type> GetTypes(Type interfaceType) 
+		{
+			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			
+			Dictionary<AssemblyType, Type[]> assemblyTypes = new Dictionary<AssemblyType, Type[]>();
+			List<Type> types = new List<Type>();
+			for (int i = 0; i < assemblies.Length; i++) 
+			{
+				AssemblyType? assemblyType = GetAssemblyType(assemblies[i].GetName().Name);
+				
+				if (assemblyType != null) 
+				{
+					assemblyTypes.Add((AssemblyType) assemblyType, assemblies[i].GetTypes());
+				}
+			}
+			
+			AddTypesFromAssembly(assemblyTypes[AssemblyType.AssemblyCSharp], types, interfaceType);
+			if (assemblyTypes.ContainsKey(AssemblyType.AssemblyCSharpFirstPass)) AddTypesFromAssembly(assemblyTypes[AssemblyType.AssemblyCSharpFirstPass], types, interfaceType);
+			
+			return types;
+		}
+	}
+}
